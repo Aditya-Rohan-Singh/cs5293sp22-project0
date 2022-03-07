23b1020a18f1cefccaa488d8a412d26eccfe1044
Aditya-Rohan-Singh <aditya.rohan.singh-1@ou.edu>
2022-03-07 19:53:45 +0000

    v0.3

--- project0/display_table.py
+++ project0/display_table.py
@@ -1,7 +1,5 @@
 import sqlite3
 
-
-
 def status(db_name):
 
     conn=None;
@@ -11,4 +9,9 @@ def status(db_name):
     select_statement = 'select nature, count(nature) from incidents group by nature order by nature ASC;'
     data_retrived = cursor.execute(select_statement).fetchall()
     return(data_retrived)
+    conn.close()
+
+if __name__ == '__main__':
+    db_name='normanpd.db'
+    status(db_name)
 
