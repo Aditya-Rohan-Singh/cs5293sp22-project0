974b9538dd39e867a5e204e05e0be80b09a561ee
Aditya-Rohan-Singh <aditya.rohan.singh-1@ou.edu>
2022-03-07 20:48:01 +0000

    v0.3.1 removed commented code

--- project0/main.py
+++ project0/main.py
@@ -3,15 +3,7 @@
 import argparse
 import project0
 from project0 import extract_data, fetch_data, createdb, insert_data, status
-#import extractdata
-#from extractdata import extract_data
-#import fetchdata
-#from fetchdata import fetch_data
 import re
-#import create_table
-#from create_table import createdb, insert_data
-#import display_table
-#from display_table import status
 
 def main(url):
     
