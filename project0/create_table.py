3cfbbd95bab02a18b0aa0dfd112acb7d57417315
Aditya-Rohan-Singh <aditya.rohan.singh-1@ou.edu>
2022-03-07 18:28:58 +0000

    v0.2

--- project0/create_table.py
+++ project0/create_table.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+import csv
 
 def database_connect(db_name):
     conn=None;
@@ -11,7 +12,14 @@ def database_connect(db_name):
 
 
 
-def create_table(conn):
+def create_table():
+    db_name='normanpd.db'
+    conn=None;
+    try:
+        conn=sqlite3.Connection(db_name)
+    except Error as e:
+        print(e)
+
     table_sql="""create table incidents (
     incident_time TEXT,
     incident_number TEXT,
@@ -25,11 +33,31 @@ def create_table(conn):
         connection.execute(table_sql)
     except Error as e:
         print(e)
+    conn.close()
 
+def insert_data(db_name)#, incidents):
+    conn=None;
+    try:
+        conn=sqlite3.Connection(db_name)
+    except Error as e:
+        print(e)
 
+    data = open('extracted_data.csv','r')
+    read_data=csv.DictReader(data)
+    to_db = [(i['incident_time'], i['incident_number'], i['incident_location'], i['nature'], i['incident_ori']) for i in read_data]
+    insert_statement = "insert into incidents (incident_time, incident_number, incident_location, nature, incident_ori) values (?,?,?,?,?)"
+    try:
+        connection=conn.cursor()
+        #connection.execute()
+        connection.executemany(insert_statement,to_db)
+        conn.commit()
+        conn.close()
+    except Error as e:
+        print(e)
 
 
 if __name__=='__main__':
     db_name="normanpd.db"
     conn=database_connect(db_name)
     create_table(conn)
+    insert_data(conn)
--- project0/main.py
+++ project0/main.py
@@ -2,13 +2,22 @@
 
 import argpass
 import project0
-
+import extractdata
+from extractdata import extract_data
+import fetchdata
+from fetchdata import fetch_data
 
 
 def main(url):
     
     #Download data from Provided URL
-    Incident_data_raw=project0.fetchincidents(url)
+    Incident_data_raw=fetch_data(url)
+
+    #Extract data from the downloaded data and create csv file
+    Extracted_data=extract_data(Incident_data_raw)
+
+    print(Extracted_data)
+
 
 if __name__ == '__main__':
         parser = argparse.ArgumentParser()
@@ -16,3 +25,5 @@ if __name__ == '__main__':
         args = parser.parse_args()
         if args.incident_url:
             main(args.incident_incdident_url)
+
+        
